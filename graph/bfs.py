151d23f8e1316b39e26b3eb0a09c39dd51c740fd
cgwelcome <charlesgao.c@gmail.com>
2019-10-25 17:12:35 -0400

    Add BFS

--- graph/bfs.py
+++ graph/bfs.py
@@ -1,14 +1,37 @@
-from collections import defaultdict, namedtuple
+from collections import defaultdict, namedtuple, deque
 
-Edge = namedtuple('Edge', ['src', 'dest'])
+def bfs(graph, src):
+    discovered = defaultdict(bool)
+    q = deque()
+    discovered[src] = True
+    q.append(src)
+    while len(q) > 0:
+        node = q.popleft()
+        print(node)
+        for adj in graph.adj[node]:
+            if not discovered[adj]:
+                discovered[adj] = True
+                q.append(adj)
+
 
 class Graph:
-    def __init__(self, edges):
+    def __init__(self, edges=[]):
         self.adj = defaultdict(list)
         for edge in edges:
             self.add_edge(edge)
 
-    def add_edge(self):
-        self.adj[edge.dst].append(edge.src)
-        self.adj[edge.src].append(edge.dst)
+    def add_edge(self, src, dest):
+        self.adj[dest].append(src)
+        self.adj[src].append(dest)
 
+if __name__ == '__main__':
+    g = Graph()
+    g.add_edge(1, 2)
+    g.add_edge(1, 3)
+    g.add_edge(2, 4)
+    g.add_edge(2, 5)
+    g.add_edge(3, 3)
+    g.add_edge(4, 5)
+    g.add_edge(4, 6)
+    g.add_edge(5, 6)
+    bfs(g, 2)
